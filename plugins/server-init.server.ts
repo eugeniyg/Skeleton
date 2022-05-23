2e0ef4df7b51b9faf6104e2f3eb1686f8e1afa8f
Dmytro Tarhan <d.tarhan@firstsport.io>
2022-05-23 14:31:32 +0300

    PLT-119-authorization

--- plugins/server-init.server.ts
+++ plugins/server-init.server.ts
@@ -4,13 +4,10 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   if (process.server) {
     const { getProfileData } = useUserStore();
     const bearer = useCookie('bearer');
-    console.log('Token:', bearer.value);
     if (bearer.value) {
-      console.log('Start Request');
       try {
         await getProfileData();
       } catch (error) {
-        console.log('Request Error:', error);
         if (error.response?.status === 401) {
           const { refreshToken } = useAuthApi();
           const { setToken } = useUserStore();
