93bf3f9dedad0af22a6805be05bd67000ec2a210
Dmytro Tarhan <d.tarhan@firstsport.io>
2022-05-30 13:16:32 +0300

    PLT-142-profile

--- CORE/apiInstance.ts
+++ CORE/apiInstance.ts
@@ -1,10 +1,14 @@
 export const useFetchInstance = (url:string, options?:any):Promise<any> => {
   const baseURL = process.server && process.env.API_BASE_URL ? process.env.API_BASE_URL : '';
-  const newOptions = { ...options, baseURL };
+  const newOptions = { ...options, baseURL, 'Content-Type': '' };
 
   const token = useCookie('bearer');
   if (token.value) {
-    newOptions.headers = { ...options?.headers, Authorization: `Bearer ${token.value}` };
+    newOptions.headers = {
+      ...options?.headers,
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token.value}`,
+    };
   }
   return $fetch(url, newOptions);
 };
