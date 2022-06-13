4572e70dd3adf4e74d183001f01db283d1741402
Dmytro Tarhan <d.tarhan@firstsport.io>
2022-06-13 13:53:04 +0300

    PPBE-188-deposit-modal transfer all cards

---
+++ components/card/category/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card-category">
+    <div class="cover"/>
+
+    <div class="title">
+      <atomic-icon :id="props.title.icon"/>{{ props.title.text }}
+    </div>
+
+    <div class="sub-title">{{ props.subTitle }}</div>
+
+    <button-base
+      type="primary"
+      size="md"
+      tag-name="a"
+      :href="props.action.href"
+    >
+      {{ props.action.title }}
+    </button-base>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    title: {
+      type: Object,
+      default: () => ({
+        icon: 'cherry',
+        text: 'text',
+      }),
+    },
+    subTitle: {
+      type: String,
+      default: 'sub title',
+    },
+    action: {
+      type: Object,
+      default: () => ({
+        title: 'Go to',
+        href: '#',
+      }),
+    },
+  });
+</script>
+
+<style lang="scss" src="./style.scss"/>
