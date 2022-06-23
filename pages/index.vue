62f989a19e6d0cf13e764657b67c66d0ec5cb713
Kirill <kirill@ip-192-168-0-102.eu-central-1.compute.internal>
2022-06-23 12:50:31 +0300

    feat: add promo block to home page (PPBE-395)

--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,17 @@
     <div class="categories">
       <card-category v-for="(item, itemIndex) in categories" :key="itemIndex" v-bind="item"/>
     </div>
+
+    <cards-group v-bind="promoCards">
+      <template v-slot:card="item">
+        <card-promotions v-bind="item"/>
+      </template>
+    </cards-group>
   </div>
 </template>
 
 <script setup lang="ts">
-  const { categories, benefitsCards } = useFakeStore();
+  const { categories, benefitsCards, promotionCards } = useFakeStore();
   const benCards = benefitsCards();
+  const promoCards = promotionCards();
 </script>
