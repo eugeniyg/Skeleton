a0c839face87fcd2b4e98e42cd9e71b8c5246a02
Kirill <kirill@ip-192-168-0-102.eu-central-1.compute.internal>
2022-06-24 14:23:50 +0300

    feat: add latest-winners block (PPBE-383)

--- components/card/latest-winners/index.vue
+++ components/card/latest-winners/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="card-latest-winners">
+  <nuxt-link class="card-latest-winners" :to="`${props.href}${!isLoggedIn ? '?demo=true' : '' }`">
     <div class="img" :style="backgroundImage"></div>
     <div class="title">{{ props.title }}</div>
     <div class="sub-title">{{ props.subTitle }}</div>
     <div class="items">
       <span v-for="(item, itemIndex) in props.items" :key="itemIndex" class="item">{{ item }}</span>
     </div>
-  </div>
+  </nuxt-link>
 </template>
 
 <script setup lang="ts">
+  import { storeToRefs } from 'pinia';
+
   const props = defineProps({
     src: {
       type: String,
@@ -27,7 +29,14 @@
       type: Array,
       default: () => [],
     },
+    href: {
+      type: String,
+      default: '',
+    },
   });
+
+  const profileStore = useProfileStore();
+  const { isLoggedIn } = storeToRefs(profileStore);
 
   const backgroundImage = computed(() => `background-image:url(/img${props.src})`);
 </script>
