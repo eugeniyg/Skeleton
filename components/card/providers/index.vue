50657cbe6895b6a6f241955bdf6f433695d08a17
Kirill <kirill@ip-192-168-0-102.eu-central-1.compute.internal>
2022-06-25 13:40:24 +0300

    feat: add provider cards (PPBE-382)

--- components/card/providers/index.vue
+++ components/card/providers/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-providers">
+  <div class="card-providers" @click="routeToGames(props.providerId)">
     <img class="img" :src="`/img${props.src}`" />
   </div>
 </template>
@@ -10,8 +10,17 @@
       type: String,
       required: true,
     },
-
+    providerId: {
+      type: String,
+      required: true,
+    },
   });
+
+  const router = useRouter();
+
+  const routeToGames = (providerId: string):void => {
+    router.push({ path: 'games', query: { category: 'all', provider: providerId } });
+  };
 </script>
 
 <style lang="scss" src="./style.scss"/>
