8e8377def9321e3855af6f38d055a5b091821a94
Igor Melnik <i.melnyk@firstsport.io>
2022-07-08 18:21:01 +0300

    add security and verification tab

--- pages/profile/security/index.vue
+++ pages/profile/security/index.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1 style="color: white;">Security</h1>
+  <div class="security content">
+    <div class="header">
+      <h1 class="heading">Security & Verification</h1>
+    </div>
+
+    <hr/>
+
+    <form class="form form-change">
+      <div class="row row-current">
+        <form-input-password
+          value=""
+          type="password"
+          name="password"
+          label="Current password"
+          placeholder="Enter your current password"
+          :is-required="true"
+          @submit="() => {}"
+        />
+      </div>
+
+      <div class="row row-new">
+        <form-input-password
+          value=""
+          type="password"
+          name="password"
+          @blur="() => {}"
+          @focus="() => {}"
+          @submit="() => {}"
+          :is-required="true"
+          label="New password"
+          placeholder="Enter new password"
+          :show-pass-progress="true"
+        />
+      </div>
+
+      <div class="row row-re">
+        <form-input-password
+          value=""
+          type="password"
+          name="password"
+          label="New password again"
+          placeholder="Re-enter new password"
+          :is-required="true"
+          @submit="() => {}"
+        />
+      </div>
+
+      <button-base
+        type="primary"
+        size="md"
+        tagName="div"
+        @click="() => {}"
+        :isDisabled="true"
+      >
+        Save changes
+      </button-base>
+    </form>
+  </div>
 </template>
+
+<style lang="scss" src="./style.scss"/>
