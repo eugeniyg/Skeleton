b4bb5eac51d70491f38db0e56d0f1c5d8c3bfb7f
Igor Melnik <i.melnyk@firstsport.io>
2022-07-08 20:12:18 +0300

    refactor security form

--- pages/profile/security/index.vue
+++ pages/profile/security/index.vue
@@ -7,42 +7,16 @@
     <hr/>
 
     <form class="form form-change">
-      <div class="row row-current">
+      <div v-for="field in changeFormData" :key="field.name" :class="`row row-${field.name}`">
         <form-input-password
-          value=""
+          v-model:value="field.value"
           type="password"
-          name="currentPassword"
-          label="Current password"
-          placeholder="Enter your current password"
+          :name="field.name"
+          :label="field.description"
+          :placeholder="field.placeholder"
+          :show-pass-progress="field.isShowPassProgress"
           :is-required="true"
-          @submit="() => {}"
-        />
-      </div>
-
-      <div class="row row-new">
-        <form-input-password
-          value=""
-          type="password"
-          name="newPassword"
-          @blur="() => {}"
-          @focus="() => {}"
-          @submit="() => {}"
-          :is-required="true"
-          label="New password"
-          placeholder="Enter new password"
-          :show-pass-progress="true"
-        />
-      </div>
-
-      <div class="row row-re">
-        <form-input-password
-          value=""
-          type="password"
-          name="rePassword"
-          label="New password again"
-          placeholder="Re-enter new password"
-          :is-required="true"
-          @submit="() => {}"
+          @submit="onSubmit"
         />
       </div>
 
@@ -58,5 +32,34 @@
     </form>
   </div>
 </template>
+
+<script setup lang="ts">
+  const changeFormFields = [
+    {
+      name: 'old',
+      description: 'Current password',
+      placeholder: 'Enter your current password',
+      value: '',
+    },
+    {
+      name: 'new',
+      description: 'New password',
+      placeholder: 'Enter new password',
+      isShowPassProgress: true,
+      value: '',
+    },
+    {
+      name: 'reenter',
+      description: 'New password again',
+      placeholder: 'Re-enter new password',
+      value: '',
+    },
+  ];
+
+  const changeFormData = reactive(changeFormFields);
+
+  const onSubmit = (): void => {
+  };
+</script>
 
 <style lang="scss" src="./style.scss"/>
