0ff4fbe852df648c51e14ccfaa3bcee8c1776980
Dmytro Tarhan <d.tarhan@firstsport.io>
2022-09-08 11:16:53 +0300

    PPBE-805-home-cms

---
+++ components/card/benefits/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card-benefits">
+    <atomic-icon :id="props.icon"/>
+    <h2 class="title">{{ props.label }}</h2>
+
+    <div class="card-benefits__list">
+      <div
+        class="card-benefits__item"
+        v-for="(benefit, index) in props.items"
+        :key="index"
+      >
+        <img class="icon" :src="benefit.image" alt="" />
+        <div class="title" v-html="marked.parse(benefit.title)" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { marked } from 'marked';
+
+  const props = defineProps<{
+    icon: string,
+    label: string,
+    items: {image: string, title: string }[],
+  }>();
+</script>
+
+<style lang="scss" src="./style.scss"/>
