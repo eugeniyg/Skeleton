8dfe97a1fe499d15c6ae4dff8bb4159617da2e74
Dmytro Tarhan <d.tarhan@firstsport.io>
2022-09-21 11:13:46 +0300

    PPBE-891-add-favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,27 @@
   <div :class="['main-layout', {'drawer-minimize': IS_DRAWER_COMPACT}]">
     <atomic-preloader/>
 
+    <Head>
+      <Meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no, user-scalable=0" />
+      <Link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <Link rel="manifest" href="/favicon/site.webmanifest" />
+      <Link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      <Meta name="msapplication-TileColor" content="#da532c" />
+      <Meta name="theme-color" content="#ffffff" />
+    </Head>
+
     <layout-header
       :is-logged-in="isLoggedIn"
       @logout="logout"
