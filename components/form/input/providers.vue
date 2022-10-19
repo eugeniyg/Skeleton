8f877e880a9674fbe71005e9334a05f237691942
Igor Melnik <i.melnyk@firstsport.io>
2022-10-19 11:56:20 +0300

    PPBE-979 add empty placeholder

--- components/form/input/providers.vue
+++ components/form/input/providers.vue
@@ -22,7 +22,7 @@
         </div>
         <div v-for="provider in gameProviders" :key="provider.id" class="input-providers__item">
           <label class="input-providers__label">
-            <!--<img class="input-providers__logo" :src="provider.logo" :alt="item.title">-->
+            <img class="input-providers__logo" src="/assets/svg/provider-empty-placeholder.svg" alt="">
             <span class="input-providers__title">{{ provider.name }}</span>
             <input
               type="checkbox"
