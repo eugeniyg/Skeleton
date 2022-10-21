6b93bd70053694be23a149f694e94108db55930a
Dmytro Tarhan <d.tarhan@firstsport.io>
2022-10-21 22:36:31 +0300

    PPBE-1011-locked-by-country

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <NuxtLayout>
+  <NuxtLayout :name="layout">
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+  const { countries } = useGlobalStore();
+  const { getRequestCountry } = useProjectMethods();
+
+  const headersCountry = getRequestCountry();
+  const allowedCountry = headersCountry ? countries.find((country) => country.code.toUpperCase() === headersCountry.toUpperCase()) : true;
+  const layout = allowedCountry ? 'default' : 'locked';
+</script>
--- layouts/locked.vue
+++ layouts/locked.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="locked">
-    <img class="locked__logo" src="assets/svg/logo-light.svg" alt="">
-    <img class="locked__media" src="assets/img/locked.png" alt=""/>
+    <img class="locked__logo" src="~/assets/svg/logo-light.svg" alt="">
+    <img class="locked__media" src="~/assets/img/locked.png" alt=""/>
 
     <h2 class="locked__title">Country blocked</h2>
     <h4 class="locked__sub-title">Sorry, but players from your country are not allowed to visit Slotsbet.io.</h4>
