40e9b410c9cd43b1cf534bcb829f2e45164efff6
Dmytro Tarhan <d.tarhan@firstsport.io>
2022-11-15 16:23:20 +0200

    PPBE-496-fix-bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -76,7 +76,7 @@ export default defineNuxtConfig({
   nitro: {
     devProxy: {
       '/api': {
-        target: 'https://test.dev.getplatform.tech/api',
+        target: 'https://dev.slotsbet.io/api',
         changeOrigin: true,
       },
     },
