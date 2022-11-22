55b3b92688a151636fea0225b96c270be53bfede
Igor Melnik <i.melnyk@firstsport.io>
2022-11-22 11:59:53 +0200

    PPBE-1171 add blocks (hot, providers, new) to index page

--- pages/index.vue
+++ pages/index.vue
@@ -19,6 +19,31 @@
 
     <group-turbo/>
 
+    <group-games
+      showAllBtn
+      showArrows
+      :category="sortCategory[0]"
+      @initialLoad="gamesGroupLoaded++"
+    />
+
+    <cards-group
+      v-if="providerCards.games?.length"
+      v-bind="providerCards"
+      :identity="groupContent?.providers.label"
+      :titleIcon="groupContent?.providers.icon"
+    >
+      <template v-slot:card="item">
+        <card-providers v-bind="item" />
+      </template>
+    </cards-group>
+
+    <group-games
+      showAllBtn
+      showArrows
+      :category="sortCategory[1]"
+      @initialLoad="gamesGroupLoaded++"
+    />
+
     <group-promotions/>
 
     <atomic-seo-text v-if="homeContent?.seo?.text" v-bind="homeContent.seo.text"/>
@@ -27,10 +52,16 @@
 
 <script setup lang="ts">
   import { storeToRefs } from 'pinia';
-  import { HomeContentInterface } from '~/types';
+  import { CardsGroupInterface, HomeContentInterface } from '~/types';
 
+  const { preloaderDone, preloaderStart } = useProjectMethods();
   const globalStore = useGlobalStore();
-  const { currentLocale } = storeToRefs(globalStore);
+  const gameStore = useGamesStore();
+  const fakeStore = useFakeStore();
+  const providerCards = fakeStore.providerCards();
+  const { gameCollections } = storeToRefs(gameStore);
+  const { currentLocale, globalComponentsContent } = storeToRefs(globalStore);
+  const groupContent:CardsGroupInterface|undefined = globalComponentsContent.value?.cardsGroup;
 
   const homeContentRequest = await useAsyncData(
     'homeContent',
@@ -39,4 +70,20 @@
   const homeContent: HomeContentInterface | undefined = homeContentRequest.data.value?.homePage;
   const { setPageSeo } = useProjectMethods();
   setPageSeo(homeContent?.seo);
+
+  const mainCategories = ['hot', 'new'];
+  const gamesGroupLoaded = ref<number>(0);
+
+  watch(() => gamesGroupLoaded.value, (newValue:number) => {
+    if (newValue === mainCategories.length) {
+      preloaderDone();
+    }
+  });
+
+  const sortCategory = gameCollections.value.filter((item) => mainCategories.find((el) => el === item.identity))
+    .sort((a, b) => mainCategories.map((e) => e).indexOf(a.identity) - mainCategories.map((e) => e).indexOf(b.identity));
+
+  onBeforeMount(() => {
+    preloaderStart();
+  });
 </script>
