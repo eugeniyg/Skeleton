a5a50cfbdee20e551e21b02e4905e9eb11831e73
dmytro_tarhan <d.tarhan@firstsport.io>
2023-01-24 19:32:51 +0200

    PPBE-1414-add-click-outside-directive

--- plugins/error-handling.ts
+++ plugins/error-handling.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('vue:error', async (err:any) => {
     if (err?.response?.status === 401) {
       const { logOutUser } = useProfileStore();
-      await logOutUser(false);
+      await logOutUser();
       return;
     }
 
