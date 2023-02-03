22c8aa6d59385600a43f5ca8adef279911426feb
dmytro_tarhan <d.tarhan@firstsport.io>
2023-02-03 16:49:15 +0200

    PPBE-1491-disabled-basic-auth

---
+++ assets/js/helpers/basicauth.js
@@ -0,0 +1,26 @@
+const AUTH_USERNAME = 'demo';
+const AUTH_PASSWORD = 'perunplay';
+
+export default defineEventHandler((event) => {
+    const host = event.req.headers?.host;
+
+    if (host !== 'slotsbet.io') {
+        return;
+    }
+
+    let allow = false;
+    const base64Credentials = event.req.headers?.authorization?.split(' ')?.[1];
+
+    if (base64Credentials) {
+        const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+        const [username, password] = credentials.split(':');
+
+        allow = username === AUTH_USERNAME && password === AUTH_PASSWORD;
+    }
+
+    if (!allow) {
+        event.res.statusCode = 401;
+        event.res.setHeader('WWW-Authenticate', 'Basic realm="Login"');
+        event.res.end('Unauthorized');
+    }
+});
