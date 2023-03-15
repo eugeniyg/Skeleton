f94af778d658e2f326a550ecee3473982d84d3a8
dmytro_tarhan <d.tarhan@firstsport.io>
2023-03-15 17:32:52 +0200

    PPBE-1637-add-content-for-currency-dropdown

--- server/routes/verify/[confirmCode].ts
+++ server/routes/verify/[confirmCode].ts
@@ -1,17 +1,12 @@
 import { useCoreConfirmApi } from '@platform/frontend-core';
 
 export default defineEventHandler(async (event) => {
-    console.log('Confirm Code: ', event.context.params?.confirmCode);
     if (event.context.params?.confirmCode) {
         const { confirmProfile } = useCoreConfirmApi();
         try {
-            console.log('before');
             await confirmProfile(event.context.params?.confirmCode);
-            console.log('after');
             return sendRedirect(event, '/?confirm=true', 302);
         } catch (err) {
-            console.log('catch');
-            console.log('err');
             return sendRedirect(event, '/', 302);
         }
     } else return sendRedirect(event, '/', 302);
