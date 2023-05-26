47afef77b573c32a9f778f24aa3708972a8f3dae
dmytro_tarhan <d.tarhan@firstsport.io>
2023-05-26 18:05:08 +0300

    change skeleton

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,8 @@ export default defineNuxtConfig({
   },
   modules: [
     '@pinia/nuxt',
-    '@platform/frontend-core'
+    '@platform/frontend-core',
+    '@nuxt/content'
   ],
   components: {
     dirs: [
