49186958c7c30cad2436c9cf6b26fc113f7039fb
Igor Melnik <i.melnyk@firstsport.io>
2023-06-26 13:53:35 +0300

    TPBE-2273-add-email-builder

---
+++ builder/email-builder/Gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const inlineCss = require('gulp-inline-css');
+
+gulp.task('default', function () {
+  return gulp.src('./src/*.html')
+    .pipe(inlineCss({
+      applyStyleTags: true,
+      applyLinkTags: true,
+      removeStyleTags: true,
+      removeLinkTags: true
+    }))
+    .pipe(gulp.dest('build/'));
+});
