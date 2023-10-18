c7080f1886b61ddc94278a42606ae6553f66b530
Igor Melnik <i.melnyk@firstsport.io>
2023-10-18 14:08:50 +0300

    TPBE-3114-payments-list-markup

--- pages/payments.vue
+++ pages/payments.vue
@@ -4,12 +4,25 @@
       <h5>Payments</h5>
     </div>
     
-    <div class="payments-page__description">
+    <div
+      class="payments-page__description"
+      :class="{'is-expanded': !showExpand}"
+      ref="descriptionElem"
+    >
       <p>Becoming a client of Slotsbet is simple: complete a short registration form, make your first deposit, and get
         access to an amazing collection of over 3000 high-quality Bitcoin casino games: Blackjack, Poker, Slots,
         Baccarat, and many more! From lottery to live dealer games, you can be sure that you are getting the very best
         thing currently available on the market.</p>
     </div>
+    
+    <button-base
+      class="btn-expand"
+      v-show="showExpand"
+      @click="expandDescription"
+    >
+      <span>More info</span>
+      <atomic-icon id="arrow_expand-close"/>
+    </button-base>
     
     <div class="payments-page__tabs">
       <div
@@ -40,12 +53,36 @@
   ];
   
   const selectedPaymentsTab = ref<string>(paymentsTabs[0].id);
+  const showExpand = ref(true);
+  
+  const descriptionElem = ref();
+  let resizeObserver: any;
   
   const changePaymentsTab = (tabId: string): void => {
     if (selectedPaymentsTab.value === tabId) return;
     selectedPaymentsTab.value = tabId;
-    //paymentsRequest();
   };
+  
+  const expandDescription = () => {
+    showExpand.value = false
+  };
+  
+  onMounted(() => {
+    resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if(entry.target?.scrollHeight > entry.target?.clientHeight && !showExpand.value) {
+          showExpand.value = true
+        }
+      }
+    });
+    
+    resizeObserver.observe(descriptionElem.value);
+  });
+  
+  onBeforeUnmount(() => {
+    resizeObserver.unobserve(descriptionElem.value);
+  });
+  
 </script>
 
 <style src="~/assets/styles/pages/payments.scss" lang="scss"/>
