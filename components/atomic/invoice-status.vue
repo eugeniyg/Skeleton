344f69a4711987abf58d10206ee0560b44d9add1
dmytro_tarhan <d.tarhan@firstsport.io>
2023-11-09 17:49:31 +0100

    TPBE-3279-transfer-content-constants

---
+++ components/atomic/invoice-status.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="invoice-status" :class="classes">
+    <atomic-icon :id="iconId"/>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    variant: number;
+  }>();
+
+  const items:{[index: string]:string} = {
+    '1': 'clock',
+    '2': 'done',
+    '3': 'warning',
+    '4': 'clock',
+  };
+  const iconId = computed(() => items[props.variant]);
+  const classes = computed(() => `invoice-status--variant-${props.variant}`);
+</script>
+
+<style src="~/assets/styles/components/atomic/invoice-status.scss" lang="scss" />
