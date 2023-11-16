86911e93d8332e2cf87777c11fe0cbda2964af92
dmytro_tarhan <d.tarhan@firstsport.io>
2023-11-16 12:24:24 +0100

    TPBE-3276-mayan-memory-test

--- core/assets/apiInstance.ts
+++ core/assets/apiInstance.ts
@@ -1,25 +1,25 @@
 import queryString from 'query-string';
 
 export const useFetchInstance = async (url:string, options?:any):Promise<any> => {
-  const profileStore = useProfileStore();
+  // const profileStore = useProfileStore();
   const globalStore = useGlobalStore();
-  const { isTokenExpired } = profileStore;
-  const cookieToken = useCookie('access_token', { maxAge: 60 * 60 * 24 * 30 });
-  const storeToken = profileStore.currentSessionToken;
-  const token = storeToken || cookieToken.value;
+  // const { isTokenExpired } = profileStore;
+  // const cookieToken = useCookie('access_token', { maxAge: 60 * 60 * 24 * 30 });
+  // const storeToken = profileStore.currentSessionToken;
+  // const token = storeToken || cookieToken.value;
   let serverRequestHeaders = {};
 
-  if (process.server) {
-    const requestHeaders = useRequestHeaders();
-
-    serverRequestHeaders = {
-      'User-Agent': requestHeaders['user-agent'],
-      'Accept-Language': requestHeaders['accept-language'],
-      'Accept-Encoding': requestHeaders['accept-encoding'],
-      [globalStore.countryHeaderName]: requestHeaders[globalStore.countryHeaderName],
-      Referer: requestHeaders.referer
-    };
-  }
+  // if (process.server) {
+  //   const requestHeaders = useRequestHeaders();
+  //
+  //   serverRequestHeaders = {
+  //     'User-Agent': requestHeaders['user-agent'],
+  //     'Accept-Language': requestHeaders['accept-language'],
+  //     'Accept-Encoding': requestHeaders['accept-encoding'],
+  //     [globalStore.countryHeaderName]: requestHeaders[globalStore.countryHeaderName],
+  //     Referer: requestHeaders.referer
+  //   };
+  // }
 
   const baseURL = process.server && process.env.API_BASE_URL ? process.env.API_BASE_URL : '';
   let newUrl = url;
@@ -40,21 +40,21 @@ export const useFetchInstance = async (url:string, options?:any):Promise<any> =>
     credentials: 'omit'
   };
 
-  if (token) { newOptions.headers.Authorization = `Bearer ${token}` };
-
-  if (token && isTokenExpired()) {
-    const { refreshToken } = useCoreAuthApi();
-    try {
-      const { data } = await refreshToken(newOptions);
-      profileStore.currentSessionToken = data.accessToken;
-      cookieToken.value = data.accessToken;
-      newOptions.headers.Authorization = `Bearer ${data.accessToken}`;
-    } catch {
-      profileStore.currentSessionToken = null;
-      cookieToken.value = null;
-      newOptions.headers.Authorization = undefined;
-    }
-  }
+  // if (token) { newOptions.headers.Authorization = `Bearer ${token}` };
+  //
+  // if (token && isTokenExpired()) {
+  //   const { refreshToken } = useCoreAuthApi();
+  //   try {
+  //     const { data } = await refreshToken(newOptions);
+  //     profileStore.currentSessionToken = data.accessToken;
+  //     cookieToken.value = data.accessToken;
+  //     newOptions.headers.Authorization = `Bearer ${data.accessToken}`;
+  //   } catch {
+  //     profileStore.currentSessionToken = null;
+  //     cookieToken.value = null;
+  //     newOptions.headers.Authorization = undefined;
+  //   }
+  // }
 
   return await $fetch(newUrl, newOptions);
 }
--- plugins/server-init.server.ts
+++ plugins/server-init.server.ts
@@ -7,7 +7,7 @@ export default defineNuxtPlugin(async ():Promise<any> => {
     getGlobalContent,
     getRequestCountry,
     getSettingsConstants,
-    pagesWithoutLocale,
+    // pagesWithoutLocale,
   } = useGlobalStore();
   const { getGameProviders, getGameCollections } = useGamesStore();
 
@@ -20,49 +20,49 @@ export default defineNuxtPlugin(async ():Promise<any> => {
     getSettingsConstants()
   ]);
 
-  const checkLanguage = ():string|undefined => {
-    const cookieLanguage = useCookie('user-language');
-    const globalStore = useGlobalStore();
-    const defaultLocale = globalStore.locales.find(locale => locale.isDefault);
-    const route = useRoute();
-    const routeLocale = route.params.locale as string;
+  // const checkLanguage = ():string|undefined => {
+  //   const cookieLanguage = useCookie('user-language');
+  //   const globalStore = useGlobalStore();
+  //   const defaultLocale = globalStore.locales.find(locale => locale.isDefault);
+  //   const route = useRoute();
+  //   const routeLocale = route.params.locale as string;
+  //
+  //   if (!cookieLanguage.value) {
+  //     const geoCountry = globalStore.countries.find(country => country.code.toUpperCase() === globalStore.headerCountry?.toUpperCase());
+  //     if (geoCountry) cookieLanguage.value = geoCountry.locale;
+  //     else cookieLanguage.value = routeLocale || defaultLocale?.code;
+  //   }
+  //
+  //   let needChangeLanguage = false;
+  //   if (route.name && routeLocale) {
+  //     needChangeLanguage = routeLocale !== cookieLanguage.value;
+  //   } else if (route.name && !routeLocale) {
+  //     needChangeLanguage = defaultLocale?.code !== cookieLanguage.value;
+  //   }
+  //
+  //   if (needChangeLanguage && !pagesWithoutLocale.includes(route.name as string)) {
+  //     const fullPathWithoutLocale = routeLocale ? route.fullPath.substring(routeLocale.length + 1) : route.fullPath;
+  //     const routePath = `${fullPathWithoutLocale === '/' ? '' : fullPathWithoutLocale}`;
+  //     if (cookieLanguage.value === defaultLocale?.code) return routePath;
+  //     return `/${cookieLanguage.value}${routePath}`;
+  //   } return undefined;
+  // };
 
-    if (!cookieLanguage.value) {
-      const geoCountry = globalStore.countries.find(country => country.code.toUpperCase() === globalStore.headerCountry?.toUpperCase());
-      if (geoCountry) cookieLanguage.value = geoCountry.locale;
-      else cookieLanguage.value = routeLocale || defaultLocale?.code;
-    }
-
-    let needChangeLanguage = false;
-    if (route.name && routeLocale) {
-      needChangeLanguage = routeLocale !== cookieLanguage.value;
-    } else if (route.name && !routeLocale) {
-      needChangeLanguage = defaultLocale?.code !== cookieLanguage.value;
-    }
-
-    if (needChangeLanguage && !pagesWithoutLocale.includes(route.name as string)) {
-      const fullPathWithoutLocale = routeLocale ? route.fullPath.substring(routeLocale.length + 1) : route.fullPath;
-      const routePath = `${fullPathWithoutLocale === '/' ? '' : fullPathWithoutLocale}`;
-      if (cookieLanguage.value === defaultLocale?.code) return routePath;
-      return `/${cookieLanguage.value}${routePath}`;
-    } return undefined;
-  };
-
-  const redirectUrl = checkLanguage();
-  if (redirectUrl !== undefined) {
-    return navigateTo(redirectUrl, { replace: true });
-  }
+  // const redirectUrl = checkLanguage();
+  // if (redirectUrl !== undefined) {
+  //   return navigateTo(redirectUrl, { replace: true });
+  // }
 
   if (process.env.NODE_ENV === 'development') {
     const globalStore = useGlobalStore();
     globalStore.baseApiUrl = process.env.API_BASE_URL || '';
   }
 
-  const { getSessionToken } = useProfileStore();
-  const sessionToken = getSessionToken();
+  // const { getSessionToken } = useProfileStore();
+  // const sessionToken = getSessionToken();
 
-  const { getProfileData } = useProfileStore();
-  const { getUserAccounts } = useWalletStore();
+  // const { getProfileData } = useProfileStore();
+  // const { getUserAccounts } = useWalletStore();
 
   const globalRequests = Promise.all([
     getGameProviders(),
@@ -70,17 +70,19 @@ export default defineNuxtPlugin(async ():Promise<any> => {
     getGlobalContent()
   ]);
 
-  if (sessionToken) {
-    const profileRequests = Promise.all([
-      getProfileData(),
-      getUserAccounts(),
-    ]);
+  await globalRequests;
 
-    await Promise.allSettled([
-      globalRequests,
-      profileRequests,
-    ]);
-  } else {
-    await globalRequests;
-  }
+  // if (sessionToken) {
+  //   const profileRequests = Promise.all([
+  //     getProfileData(),
+  //     getUserAccounts(),
+  //   ]);
+  //
+  //   await Promise.allSettled([
+  //     globalRequests,
+  //     profileRequests,
+  //   ]);
+  // } else {
+  //   await globalRequests;
+  // }
 });
