ce1a70b5f15cd26afc018de96cfeac418240e00c
dmytro_tarhan <d.tarhan@firstsport.io>
2023-11-21 14:47:28 +0100

    TPBE-3003-improve-site-speed

--- plugins/server-init.server.ts
+++ plugins/server-init.server.ts
@@ -7,51 +7,49 @@ export default defineNuxtPlugin(async ():Promise<any> => {
     getGlobalContent,
     getRequestCountry,
     getSettingsConstants,
-    //pagesWithoutLocale,
+    pagesWithoutLocale,
   } = useGlobalStore();
   const { getGameProviders, getGameCollections } = useGamesStore();
 
   getRequestCountry();
 
   await Promise.all([
-    getCurrencies(),
     getLocales(),
     getCountries(),
-    getSettingsConstants()
   ]);
 
-  // const checkLanguage = ():string|undefined => {
-  //   const cookieLanguage = useCookie('user-language');
-  //   const globalStore = useGlobalStore();
-  //   const defaultLocale = globalStore.locales.find(locale => locale.isDefault);
-  //   const route = useRoute();
-  //   const routeLocale = route.params.locale as string;
-  //
-  //   if (!cookieLanguage.value) {
-  //     const geoCountry = globalStore.countries.find(country => country.code.toUpperCase() === globalStore.headerCountry?.toUpperCase());
-  //     if (geoCountry) cookieLanguage.value = geoCountry.locale;
-  //     else cookieLanguage.value = routeLocale || defaultLocale?.code;
-  //   }
-  //
-  //   let needChangeLanguage = false;
-  //   if (route.name && routeLocale) {
-  //     needChangeLanguage = routeLocale !== cookieLanguage.value;
-  //   } else if (route.name && !routeLocale) {
-  //     needChangeLanguage = defaultLocale?.code !== cookieLanguage.value;
-  //   }
-  //
-  //   if (needChangeLanguage && !pagesWithoutLocale.includes(route.name as string)) {
-  //     const fullPathWithoutLocale = routeLocale ? route.fullPath.substring(routeLocale.length + 1) : route.fullPath;
-  //     const routePath = `${fullPathWithoutLocale === '/' ? '' : fullPathWithoutLocale}`;
-  //     if (cookieLanguage.value === defaultLocale?.code) return routePath;
-  //     return `/${cookieLanguage.value}${routePath}`;
-  //   } return undefined;
-  // };
+  const checkLanguage = ():string|undefined => {
+    const cookieLanguage = useCookie('user-language');
+    const globalStore = useGlobalStore();
+    const defaultLocale = globalStore.locales.find(locale => locale.isDefault);
+    const route = useRoute();
+    const routeLocale = route.params.locale as string;
 
-  // const redirectUrl = checkLanguage();
-  // if (redirectUrl !== undefined) {
-  //   return navigateTo(redirectUrl, { replace: true });
-  // }
+    if (!cookieLanguage.value) {
+      const geoCountry = globalStore.countries.find(country => country.code.toUpperCase() === globalStore.headerCountry?.toUpperCase());
+      if (geoCountry) cookieLanguage.value = geoCountry.locale;
+      else cookieLanguage.value = routeLocale || defaultLocale?.code;
+    }
+
+    let needChangeLanguage = false;
+    if (route.name && routeLocale) {
+      needChangeLanguage = routeLocale !== cookieLanguage.value;
+    } else if (route.name && !routeLocale) {
+      needChangeLanguage = defaultLocale?.code !== cookieLanguage.value;
+    }
+
+    if (needChangeLanguage && !pagesWithoutLocale.includes(route.name as string)) {
+      const fullPathWithoutLocale = routeLocale ? route.fullPath.substring(routeLocale.length + 1) : route.fullPath;
+      const routePath = `${fullPathWithoutLocale === '/' ? '' : fullPathWithoutLocale}`;
+      if (cookieLanguage.value === defaultLocale?.code) return routePath;
+      return `/${cookieLanguage.value}${routePath}`;
+    } return undefined;
+  };
+
+  const redirectUrl = checkLanguage();
+  if (redirectUrl !== undefined) {
+    navigateTo(redirectUrl);
+  }
 
   if (process.env.NODE_ENV === 'development') {
     const globalStore = useGlobalStore();
@@ -65,6 +63,8 @@ export default defineNuxtPlugin(async ():Promise<any> => {
   const { getUserAccounts } = useWalletStore();
 
   const globalRequests = Promise.all([
+    getCurrencies(),
+    getSettingsConstants(),
     getGameProviders(),
     getGameCollections(),
     getGlobalContent()
