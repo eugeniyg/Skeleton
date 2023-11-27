b602aed880129a21f972449a78bbe19ff7b1504d
dmytro_tarhan <d.tarhan@firstsport.io>
2023-11-27 12:03:44 +0100

    TPBE-3394-use-nuxt-data

--- pages/profile/bonuses.vue
+++ pages/profile/bonuses.vue
@@ -42,18 +42,34 @@
   const { getPlayerBonuses, getPlayerFreeSpins } = bonusStore;
   const { activePlayerBonuses, activePlayerFreeSpins } = storeToRefs(bonusStore);
 
-  const [currentLocaleContentResponse, defaultLocaleContentResponse] = await Promise.allSettled([
-    useAsyncData('currentLocaleProfileBonusesContent', () => queryContent(currentLocale.value?.code as string, 'profile', 'bonuses').findOne()),
-    currentLocale.value?.isDefault ? Promise.reject('Current locale is default locale!')
-      : useAsyncData('defaultLocaleProfileBonusesContent', () => queryContent(defaultLocale.value?.code as string, 'profile', 'bonuses').findOne()),
-    useAsyncData('updatePlayerBonuses', getPlayerBonuses),
-    useAsyncData('updatePlayerFreeSpins', getPlayerFreeSpins),
-  ]);
+  let bonusesContent: Maybe<IProfileBonuses>;
+  let defaultLocaleBonusesContent: Maybe<IProfileBonuses>;
+  const [nuxtCurrentLocaleData, nuxtDefaultLocaleData] = [
+    useNuxtData('currentLocaleProfileBonusesContent'),
+    useNuxtData('defaultLocaleProfileBonusesContent')
+  ]
 
-  const { currentLocaleData, defaultLocaleData } = getLocalesContentData(currentLocaleContentResponse, defaultLocaleContentResponse);
-  const bonusesContent: Maybe<IProfileBonuses> = currentLocaleData;
-  const defaultLocaleBonusesContent: Maybe<IProfileBonuses> = defaultLocaleData;
+  if (nuxtCurrentLocaleData.data.value || nuxtDefaultLocaleData.data.value) {
+    bonusesContent = nuxtCurrentLocaleData.data.value;
+    defaultLocaleBonusesContent = nuxtDefaultLocaleData.data.value;
+  } else {
+    const [currentLocaleContentResponse, defaultLocaleContentResponse] = await Promise.allSettled([
+      useAsyncData('currentLocaleProfileBonusesContent', () => queryContent(currentLocale.value?.code as string, 'profile', 'bonuses').findOne()),
+      currentLocale.value?.isDefault ? Promise.reject('Current locale is default locale!')
+        : useAsyncData('defaultLocaleProfileBonusesContent', () => queryContent(defaultLocale.value?.code as string, 'profile', 'bonuses').findOne())
+    ]);
+
+    const { currentLocaleData, defaultLocaleData } = getLocalesContentData(currentLocaleContentResponse, defaultLocaleContentResponse);
+    bonusesContent = currentLocaleData;
+    defaultLocaleBonusesContent = defaultLocaleData;
+  }
+
   setPageSeo(bonusesContent?.seo);
+
+  onMounted(() => {
+    getPlayerBonuses();
+    getPlayerFreeSpins();
+  })
 </script>
 
 <style src="~/assets/styles/pages/profile/bonuses.scss" lang="scss" />
