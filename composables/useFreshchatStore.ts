1786c2fd0521f571bc67aa1cfbd8d6d410b472ad
inf <alexey.nagirnyak@gmail.com>
2023-12-05 15:31:13 +0200

    TPBE-3436: Freshchat investigate

--- composables/useFreshchatStore.ts
+++ composables/useFreshchatStore.ts
@@ -6,66 +6,33 @@ interface IFreshchatState {
 
 export const useFreshchatStore = defineStore('freshchatStore', {
   state: (): IFreshchatState => ({
-    newMessages: 0
+    newMessages: 0,
+    initialize: true,
   }),
 
   actions: {
-    setUserChatData(): void {
+    getWidgetAttributes() {
       const { profile, isLoggedIn } = useProfileStore();
-      const segmentsArr = profile?.segments.map(segment => segment.name);
-      const profileSegments = segmentsArr?.length ? segmentsArr.join(', ') : null;
 
-      const storeData = localStorage.getItem('restore');
-      const storeRestoreList: { externalId: string, restoreId: string }[] = storeData ? JSON.parse(storeData) : [];
-      const userRestore = storeRestoreList.find(restoreData => restoreData.externalId === profile?.id)?.restoreId;
-
-      const { activePlayerBonuses, activePlayerFreeSpins } = useBonusStore();
-
-      console.log('isLoggedIn: ', isLoggedIn);
-      if (isLoggedIn) {
-        window.fcWidgetMessengerConfig = {
-          externalId: profile?.id,
-          restoreId: userRestore || null
-        };
-
-        window.fcWidget.user.setProperties({
-          firstName: profile?.firstName,
-          lastName: profile?.lastName,
-          email: profile?.email,
-          cf_segments: profileSegments,
-          cf_active_bonuses: `${!!(activePlayerBonuses?.length || activePlayerFreeSpins?.length)}`,
-        });
+      if(!isLoggedIn) {
+        return {};
       }
 
-      window.fcWidget.on('user:created', function({ status, data }:any) {
-        const { isLoggedIn } = useProfileStore();
-        if (isLoggedIn && status === 200 && data.restoreId) {
-          const stringifyData = JSON.stringify(
-            [...storeRestoreList, { externalId: profile?.id, restoreId: data.restoreId }]
-          )
-          localStorage.setItem('restore', stringifyData);
-        }
-      });
+      const userRestore = localStorage.getItem('__freshchat_restore_id');
 
-      window.fcWidget.on('user:cleared', function() {
-        console.log('user cleared');
-        window.fcWidget.destroy();
-      });
-
-      window.fcWidget.on('unreadCount:notify', function({ count }: { count: number }) {
-        const freshchatStore = useFreshchatStore();
-        freshchatStore.newMessages = count;
-      });
-
-      window.fcWidget.on('widget:destroyed', async function() {
-        console.log('destroyed');
-        const freshchatStore = useFreshchatStore();
-        freshchatStore.initChat();
-      });
+      return {
+        externalId: profile.id,
+        restoreId: userRestore,
+      }
     },
 
-    initChat():void {
-      this.setUserChatData();
+    initChat() {
+      if(!this.initialize) {
+        return;
+      }
+
+      this.initialize = false;
+
       window.fcWidget.init({
         config: {
           headerProperty: {
@@ -73,24 +40,123 @@ export const useFreshchatStore = defineStore('freshchatStore', {
           }
         },
         token: "76ea1b8c-4e71-406f-bdb9-5c9d04dd11b6",
-        host: "https://turbostars-org-90b688ba9fde9ad17017532.freshchat.com"
+        host: "https://turbostars-org-90b688ba9fde9ad17017532.freshchat.com",
+        ...this.getWidgetAttributes(),
+      });
+
+      window.fcWidget.on('widget:loaded', () => {
+        console.log('widget loaded');
+        this.setUserChatData();
       })
+
+      window.fcWidget.on('widget:destroyed', () => {
+        this.initChat();
+      });
+
+      window.fcWidget.on('user:created', function({ status, data }:any) {
+        console.log('user created', data);
+        const { isLoggedIn } = useProfileStore();
+
+        if(isLoggedIn && data?.restoreId) {
+          localStorage.setItem('__freshchat_restore_id', data.restoreId);
+        }
+      });
+
+      window.fcWidget.on('unreadCount:notify', ({ count }: { count: number }) => {
+        this.newMessages = count;
+      });
     },
 
-    async clearChat(): Promise<void> {
-      try {
-        await window.fcWidget.user.clear()
-      } catch {
-        console.log('Chat not cleared, chat empty!');
+    setUserChatData(): void {
+      const { profile, isLoggedIn } = useProfileStore();
+      const segmentsArr = profile?.segments.map(segment => segment.name);
+      const profileSegments = segmentsArr?.length ? segmentsArr.join(', ') : null;
+
+      const { activePlayerBonuses, activePlayerFreeSpins } = useBonusStore();
+
+      if (isLoggedIn) {
+        window.fcWidget.user.setProperties({
+          firstName: profile?.firstName,
+          lastName: profile?.lastName,
+          email: profile?.email,
+          meta: {
+            cf_segments: profileSegments,
+            cf_active_bonuses: `${!!(activePlayerBonuses?.length || activePlayerFreeSpins?.length)}`,
+          },
+        });
       }
     },
+
+    // initChat():void {
+    //   this.initializeWidget();
+    //   this.setUserChatData();
+
+    //   window.fcWidget.on('user:created', function({ status, data }:any) {
+    //     console.log('user created', data);
+    //     const { isLoggedIn } = useProfileStore();
+
+    //     if(isLoggedIn && data?.restoreId) {
+    //       localStorage.setItem('__freshchat_restore_id', data.restoreId);
+    //     }
+
+    //     // console.log('cfg', window.fcWidget.getConfig());
+    //     // if (isLoggedIn && status === 200 && data.restoreId) {
+    //     //   const stringifyData = JSON.stringify(
+    //     //     [...storeRestoreList, { externalId: profile?.id, restoreId: data.restoreId }]
+    //     //   )
+    //     //   localStorage.setItem('restore', stringifyData);
+    //     // }
+    //   });
+
+    //   window.fcWidget.on('user:cleared', () => {
+    //     console.log('user cleared');
+    //     this.setUserChatData();
+    //     // window.fcWidget.destroy();
+    //   });
+
+    //   window.fcWidget.on('widget:destroyed', () => {
+    //     // const store = useFreshchatStore();
+
+    //     // if(state.reinitialize) {
+    //     //   this.cr
+    //     // }
+
+    //     console.log('widget destroyed');
+    //   })
+
+    //   window.fcWidget.on('unreadCount:notify', function({ count }: { count: number }) {
+    //     const freshchatStore = useFreshchatStore();
+    //     freshchatStore.newMessages = count;
+    //   });
+
+    //   // if(window.fcWidget && !window.fcWidget.isInitialized()) {
+    //   //   console.log('reinitialize');
+    //   //   // window.fcWidget.init();
+    //   // }
+
+    //   // window.fcWidget.init({
+    //   //   config: {
+    //   //     headerProperty: {
+    //   //       hideChatButton: true
+    //   //     }
+    //   //   },
+    //   //   token: "76ea1b8c-4e71-406f-bdb9-5c9d04dd11b6",
+    //   //   host: "https://turbostars-org-90b688ba9fde9ad17017532.freshchat.com"
+    //   // })
+    // },
 
     async updateChat():Promise<void> {
+      console.log('update');
+      this.initialize = true;
+
       try {
         await window.fcWidget.user.clear()
-      } catch {
-        window.fcWidget.destroy();
+      } catch (e) {
+        console.log(e);
+        // do nothing
       }
+
+      window.fcWidget.destroy();
     },
   }
 });
