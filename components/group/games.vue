51b63920cfbde08e9093abeea4ee22acaf0ea527
dmytro_tarhan <d.tarhan@firstsport.io>
2023-12-15 11:19:24 +0100

    TPBE-3520-fix-infinity-game-loading

--- components/group/games.vue
+++ components/group/games.vue
@@ -50,7 +50,7 @@
         <div v-for="n in 9" :key="n" class="card-base"/>
       </template>
 
-      <div class="load-more" ref="loadMore"/>
+      <div class="load-more" ref="loadMore" @inview="moreGames"/>
     </div>
   </div>
 </template>
