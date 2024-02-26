95c4e3b9ba27c291c34c133ab80686d8de1460d9
Igor Melnik <i.melnyk@firstsport.io>
2024-02-26 14:55:56 +0200

    TPBE-4264-custom-sidebar-menu-icons

--- components/nav/list.vue
+++ components/nav/list.vue
@@ -6,22 +6,27 @@
         Number(getContent(layoutData, defaultLocaleLayoutData, 'siteSidebar.topMenu.integratedElement.order')) === index"
         v-bind="getContent(layoutData, defaultLocaleLayoutData, 'siteSidebar.topMenu.integratedElement')"
       />
-
+      
       <atomic-menu-category v-if="listItem?.items?.length" v-bind="listItem"/>
-
+      
       <div
         v-else
         class="link"
         @click="defineCurrentAction(listItem.url)"
         :class="{'is-active': route.fullPath === localizePath(listItem.url)}"
       >
-        <atomic-icon :id="listItem.icon"/>
+        <atomic-svg
+          v-if="listItem?.displayCustomIcon && listItem?.customIcon"
+          :src="listItem?.customIcon"
+        />
+        <atomic-icon  v-else :id="listItem?.icon ? listItem?.icon : 'dot-md'"/>
+        
         <div class="text">{{ listItem.label }}</div>
         <client-only>
           <div v-if="listItem.counter" class="counter">{{ listItem.counter }}</div>
         </client-only>
       </div>
-
+      
       <list-games
         v-if="listItem.gameList?.length"
         :items="listItem.gameList.map((game: any) => game.gameIdentity).slice(0, 4)"
@@ -32,25 +37,25 @@
 
 <script setup lang="ts">
   import { storeToRefs } from 'pinia';
-
+  
   const props = defineProps<{
     items: any;
     isOpen?: boolean;
     accentItems?: boolean;
   }>();
-
+  
   const router = useRouter();
   const route = useRoute();
   const profileStore = useProfileStore();
   const { isLoggedIn } = storeToRefs(profileStore);
-
+  
   const { localizePath } = useProjectMethods();
   const {
     layoutData,
     defaultLocaleLayoutData
   } = useGlobalStore();
   const { getContent } = useProjectMethods();
-
+  
   const defineCurrentAction = (href: string): void => {
     if (!href) return;
     router.push(localizePath(href));
