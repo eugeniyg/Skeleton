30fb7e2d6f7d754ae84463ed074f4043b4746c8c
inf <alexey.nagirnyak@gmail.com>
2024-06-03 12:29:12 +0300

    TPBE-5391: service mapping

--- core/assets/apiGuestInstance.ts
+++ core/assets/apiGuestInstance.ts
@@ -1,7 +1,39 @@
 import queryString from 'query-string';
 
+const ROUTES_MAP = {
+  '/api/player': process.env.PLAYER_SERVICE_BASE_URL,
+  '/api/settings': process.env.SETTINGS_SERVICE_BASE_URL,
+  '/api/notification': process.env.NOTIFICATION_SERVICE_BASE_URL,
+  '/api/payment': process.env.PAYMENT_SERVICE_BASE_URL,
+  '/api/game': process.env.GAME_SERVICE_BASE_URL,
+  '/api/retention': process.env.RETENTION_SERVICE_BASE_URL,
+  '/api/segment': process.env.SEGMENT_SERVICE_BASE_URL,
+  '/api/wallet': process.env.WALLET_SERVICE_BASE_URL,
+  '/api/shop': process.env.SHOP_SERVICE_BASE_URL,
+}
+
+const API_BASE_URL = process.env.API_BASE_URL;
+
+const getApiUrl = (url:string) => {
+  if(!process.server) {
+    return url;
+  }
+
+  // example: /api/player/something -> http://player-service/api/public/something
+  for (const [key, value] of Object.entries(ROUTES_MAP)) {
+    if (value && url.startsWith(key)) {
+      return url.replace(key, value + '/api/public');
+    }
+  }
+
+  if(!API_BASE_URL) {
+    return url;
+  }
+
+  return API_BASE_URL + url;
+};
+
 export const useApiGuestInstance = async (url:string, options?:any):Promise<any> => {
-  const baseURL = process.server && process.env.API_BASE_URL ? process.env.API_BASE_URL : '';
   let newUrl = url;
 
   if (options?.params) {
@@ -11,11 +43,10 @@ export const useApiGuestInstance = async (url:string, options?:any):Promise<any>
 
   const newOptions = {
     ...options,
-    baseURL,
     params: undefined,
     credentials: 'omit',
     retry: 0
   };
 
-  return await $fetch(newUrl, newOptions);
+  return await $fetch(getApiUrl(newUrl), newOptions);
 }
