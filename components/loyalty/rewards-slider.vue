5432f5ce06de06ba3dbd69e9a18bb751dde481ce
Igor Melnik <i.melnyk@firstsport.io>
2024-07-09 16:34:50 +0300

    TPBE-5607-loyality-markup

--- components/loyalty/rewards-slider.vue
+++ components/loyalty/rewards-slider.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <div class="loyalty-rewards-slider" ref="sliderNode">
+  <div
+    class="loyalty-rewards-slider__wrap"
+    :class="{
+    'is-centered': isCentered,
+    'is-prev-disabled': isPrevDisabled,
+    'is-next-disabled': isNextDisabled,
+    }"
+  >
+    <div
+      class="loyalty-rewards-slider"
+      ref="sliderNode"
+    >
       <div class="loyalty-rewards-slider__viewport">
         <loyalty-rewards-slider-item
           v-for="level in loyaltyLevels"
@@ -10,35 +20,12 @@
       </div>
     </div>
     
-    <div
-      v-show="levels.length > 1"
-      class="loyalty-rewards-slider__nav"
-      @mouseover="onMouseOver"
-      @mouseleave="onMouseLeave"
-    >
-      <div
-        class="loyalty-rewards-slider__btn is-prev"
-        @click="scrollPrev"
-      >
-        <atomic-icon id="arrow_expand-close"/>
-      </div>
-      
-      <div class="loyalty-rewards-slider__bullets">
-        <div
-          v-for="(item, index) in levels.length"
-          :key="item"
-          class="loyalty-rewards-slider__bullet"
-          :class="{'is-active': index === activeIndex}"
-          @click="scrollTo(index)"
-        />
-      </div>
-      
-      <div
-        class="loyalty-rewards-slider__btn is-next"
-        @click="scrollPrev"
-      >
-        <atomic-icon id="arrow_expand-close"/>
-      </div>
+    <div class="loyalty-rewards-slider__btn is-prev" @click="scrollPrev">
+      <atomic-icon id="arrow_expand-close"/>
+    </div>
+    
+    <div class="loyalty-rewards-slider__btn is-next" @click="scrollNext">
+      <atomic-icon id="arrow_expand-close"/>
     </div>
   </div>
 </template>
@@ -47,103 +34,51 @@
   const { loyaltyLevels } = useFakeStore();
   
   import emblaCarouselVue from 'embla-carousel-vue';
-  import Autoplay from 'embla-carousel-autoplay';
+  //import Autoplay from 'embla-carousel-autoplay';
   
   const activeIndex = ref<number>(0);
+  const isPrevDisabled = ref<boolean>(false);
+  const isNextDisabled = ref<boolean>(false);
+  const slidesInView = ref<number[]>([]);
   
-  const autoplayOptions = {
-    delay: 4000,
-    startDelay: 1000,
-    stopOnInteraction: false,
-    stopOnMouseEnter: false
-  };
-  
-  const levels = [
-    {
-      type: 'previous',
-      name: 'Previous',
-      image: '',
-      icon: 'flash',
-      statusPoints: '139 - 178',
-      rankBonus: {
-        label: 'Rank bonus',
-        value: 0,
-        currency: 'USDT',
-      },
-      cashBack: {
-        label: 'Cashback',
-        value: 0,
-      },
-      isActive: true,
-      tasks: [
-        {
-          label: 'Task rewards +50%',
-          icon: ''
-        },
-        {
-          label: 'Bonus for reaching a new level',
-          icon: ''
-        },
-        {
-          label: 'Monthly boost',
-          icon: ''
-        },
-        {
-          label: 'Personal VIP manager',
-          icon: ''
-        },
-      ]
-    },
-  
-  ];
+  // const autoplayOptions = {
+  //   delay: 4000,
+  //   startDelay: 1000,
+  //   stopOnInteraction: false,
+  //   stopOnMouseEnter: true
+  // };
   
   const [sliderNode, emblaApi] = emblaCarouselVue({
-    loop: false,
-    align: 'center'
-  },
-    [Autoplay(autoplayOptions)]
+      loop: false,
+      //align: 'center'
+    },
+    //[Autoplay(autoplayOptions)]
   );
   
-  const onSelectSlide = () => {
-    activeIndex.value = emblaApi.value?.selectedScrollSnap() || 0;
+  const isCentered = computed(() => isPrevDisabled.value && isNextDisabled.value);
+  
+  const togglePrevNextButtons = () => {
+    isPrevDisabled.value = !emblaApi.value?.canScrollPrev();
+    isNextDisabled.value = !emblaApi.value?.canScrollNext();
   };
   
-  const scrollTo = (index: number) => {
-    emblaApi.value?.scrollTo(index);
+  const onSelectSlide = () => {
+    togglePrevNextButtons();
   };
   
   const scrollPrev = () => {
-    if (!emblaApi.value?.canScrollPrev()) {
-      emblaApi.value?.scrollTo(emblaApi.value?.slideNodes()?.length);
-      onSelectSlide();
-    } else {
-      emblaApi.value?.scrollPrev();
-    }
+    emblaApi.value?.scrollPrev();
   };
   
   const scrollNext = () => {
-    if (!emblaApi.value?.canScrollNext()) {
-      emblaApi.value?.scrollTo(0);
-      onSelectSlide();
-    } else {
-      emblaApi.value?.scrollNext();
-    }
-  };
-  
-  const onMouseOver = () => {
-    const autoplay = emblaApi.value?.plugins()?.autoplay as any;
-    autoplay?.stop();
-  };
-  
-  const onMouseLeave = () => {
-    if (!emblaApi.value) return;
-    const autoplay = emblaApi.value?.plugins()?.autoplay as any;
-    autoplay?.play();
+    emblaApi.value?.scrollNext();
   };
   
   watchEffect(() => {
     if (emblaApi.value) {
       emblaApi.value?.on('select', onSelectSlide);
+      emblaApi.value?.on('resize', togglePrevNextButtons);
+      togglePrevNextButtons();
     }
   });
 </script>
