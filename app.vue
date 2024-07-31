c8f5ad7d2339d7c3bdb5d7c822987688022ed096
dmytro_tarhan <d.tarhan@firstsport.io>
2024-07-31 15:15:10 +0200

    TPBE-6032-add-lang-attribute

--- app.vue
+++ app.vue
@@ -5,3 +5,13 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+  const { currentLocale } = useGlobalStore();
+
+  useHead({
+    htmlAttrs: {
+      lang: currentLocale?.code
+    },
+  })
+</script>
