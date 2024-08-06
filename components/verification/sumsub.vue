19a272ba790852a5cd857fcf4385883da962bf47
dmytro_tarhan <d.tarhan@firstsport.io>
2024-08-06 11:26:42 +0200

    TPBE-6034-email-verification

---
+++ components/verification/sumsub.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="sumsub-verify">
+    <verification-sumsub-form />
+    <verification-sumsub-info />
+  </div>
+</template>
+
+<style src="~/assets/styles/components/verification/sumsub.scss" lang="scss" />
--- pages/profile/verification.vue
+++ pages/profile/verification.vue
@@ -6,52 +6,61 @@
       </h1>
     </div>
 
-    <profile-sumsub v-if="sumsubIntegrated" />
+    <div v-if="hasEmailField" class="verification__profile">
+      <verification-email />
+    </div>
+
+    <verification-sumsub v-if="sumsubIntegrated" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-import type {IProfileVerification} from '~/types';
+  import { storeToRefs } from 'pinia';
+  import type {IProfileVerification} from '~/types';
 
-const { setPageMeta, getLocalesContentData } = useProjectMethods();
-const globalStore = useGlobalStore();
-const { currentLocale, defaultLocale } = storeToRefs(globalStore);
+  const { setPageMeta, getLocalesContentData } = useProjectMethods();
+  const globalStore = useGlobalStore();
+  const { currentLocale, defaultLocale } = storeToRefs(globalStore);
+  const fieldsStore = useFieldsStore();
+  const { profileFields } = storeToRefs(fieldsStore);
+  const hasEmailField = computed(() => {
+    return !!profileFields.value.find(field => field.name === 'email');
+  })
 
-const verificationContent = ref<Maybe<IProfileVerification>>();
-const defaultLocaleVerificationContent = ref<Maybe<IProfileVerification>>();
-provide('verificationContent', verificationContent);
-provide('defaultLocaleVerificationContent', defaultLocaleVerificationContent);
+  const verificationContent = ref<Maybe<IProfileVerification>>();
+  const defaultLocaleVerificationContent = ref<Maybe<IProfileVerification>>();
+  provide('verificationContent', verificationContent);
+  provide('defaultLocaleVerificationContent', defaultLocaleVerificationContent);
 
-interface IPageContent {
-  currentLocaleData: Maybe<IProfileVerification>;
-  defaultLocaleData: Maybe<IProfileVerification>;
-}
+  interface IPageContent {
+    currentLocaleData: Maybe<IProfileVerification>;
+    defaultLocaleData: Maybe<IProfileVerification>;
+  }
 
-const setContentData = (contentData: Maybe<IPageContent>): void => {
-  verificationContent.value = contentData?.currentLocaleData;
-  defaultLocaleVerificationContent.value = contentData?.defaultLocaleData;
-  setPageMeta(verificationContent.value?.pageMeta);
-}
+  const setContentData = (contentData: Maybe<IPageContent>): void => {
+    verificationContent.value = contentData?.currentLocaleData;
+    defaultLocaleVerificationContent.value = contentData?.defaultLocaleData;
+    setPageMeta(verificationContent.value?.pageMeta);
+  }
 
-const getPageContent = async (): Promise<IPageContent> => {
-  const { data } = useNuxtData('profileVerificationContent');
-  if (data.value) return data.value;
+  const getPageContent = async (): Promise<IPageContent> => {
+    const { data } = useNuxtData('profileVerificationContent');
+    if (data.value) return data.value;
 
-  const [currentLocaleContentResponse, defaultLocaleContentResponse] = await Promise.allSettled([
-    queryContent(currentLocale.value?.code as string, 'profile', 'verification').findOne(),
-    currentLocale.value?.isDefault ? Promise.reject('Current locale is default locale!')
+    const [currentLocaleContentResponse, defaultLocaleContentResponse] = await Promise.allSettled([
+      queryContent(currentLocale.value?.code as string, 'profile', 'verification').findOne(),
+      currentLocale.value?.isDefault ? Promise.reject('Current locale is default locale!')
       : queryContent(defaultLocale.value?.code as string, 'profile', 'verification').findOne()
-  ]);
-  return getLocalesContentData(currentLocaleContentResponse, defaultLocaleContentResponse);
-}
+    ]);
+    return getLocalesContentData(currentLocaleContentResponse, defaultLocaleContentResponse);
+  }
 
-const { data: content } = await useLazyAsyncData('profileVerificationContent', () => getPageContent());
+  const { data: content } = await useLazyAsyncData('profileVerificationContent', () => getPageContent());
 
-watch(content, () => {
-  if (content.value) setContentData(content.value);
-}, { immediate: true });
+  watch(content, () => {
+    if (content.value) setContentData(content.value);
+  }, { immediate: true });
 
-const runtimeConfig = useRuntimeConfig();
-const sumsubIntegrated = !!runtimeConfig.public.sumsub?.appToken;
+  const runtimeConfig = useRuntimeConfig();
+  const sumsubIntegrated = !!runtimeConfig.public.sumsub?.appToken;
 </script>
