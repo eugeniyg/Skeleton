756faa329b6d66ba25e6a2ceea74e9bc7f3ca523
dmytro_tarhan <d.tarhan@firstsport.io>
2024-08-15 17:43:59 +0200

    TPBE-6150-restrict-page

--- layouts/geo-restricted.vue
+++ layouts/geo-restricted.vue
@@ -3,18 +3,46 @@
     <atomic-image class="geo-restricted__logo" src="/img/logo-light.svg" />
 
     <div class="geo-restricted__content">
-      <div class="geo-restricted__image"></div>
-      <h1 class="geo-restricted__title"></h1>
-      <div class="geo-restricted__description"></div>
-      <div class="geo-restricted__address"></div>
+      <atomic-image
+        class="geo-restricted__image"
+        :src="getContent(globalComponentsContent, defaultLocaleGlobalComponentsContent, 'geoRestricted.image')"
+      />
+
+      <h1 class="geo-restricted__title">
+        {{ getContent(globalComponentsContent, defaultLocaleGlobalComponentsContent, 'geoRestricted.title') }}
+      </h1>
+
+      <div
+        v-if="description"
+        class="geo-restricted__description"
+        v-html="marked.parse(description)"
+      />
+
+      <div v-if="headerCountry" class="geo-restricted__geo">
+        <atomic-image class="geo-restricted__geo-flag" :src="`/img/flags/${headerCountry.toLowerCase()}.svg`" />
+        <span class="geo-restricted__geo-country">{{ headerCountry }}</span>
+        <span class="geo-restricted__geo-ip">{{ headerIp }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  const { setPageMeta } = useProjectMethods();
-  setPageMeta({});
+  import { marked } from "marked";
+
+  const globalStore = useGlobalStore();
+  const {
+    headerCountry,
+    headerIp,
+    globalComponentsContent,
+    defaultLocaleGlobalComponentsContent
+  } = storeToRefs(globalStore);
+  const { getContent } = useProjectMethods();
+
+  const description = computed(() => {
+    return getContent(globalComponentsContent.value, defaultLocaleGlobalComponentsContent.value, 'geoRestricted.description');
+  })
 </script>
 
-<style src="~/assets/styles/layouts/locked.scss" lang="scss" />
+<style src="~/assets/styles/layouts/geo-restricted.scss" lang="scss" />
 
